6f388ef75612e54bd43e96041a9791ce17b69ee5
Thuan Luu <144054011+ThuanLuu3@users.noreply.github.com>
2026-04-02 15:59:14 -0700

    calculator2

---
+++ homework/april_2/Submission/Calculator2/Calculator2/Program.cs
@@ -0,0 +1,77 @@
+﻿Console.WriteLine("This calculator can perform 5 operation: addition (+), subtraction (-), multiplication (*), division.");
+Console.WriteLine();
+
+while (true)
+{
+    Console.WriteLine("Please type a mathematical expression, or type \"quit\" to shut down the calculator.");
+    string input = Console.ReadLine();
+    if (input.ToLower() == "quit")
+    {
+        Console.WriteLine("Goodbye!");
+        break;
+    }
+    
+    string[] parts = input.Split(' ');
+    if (parts.Length == 3)
+    {
+        double num1;
+        double num2;
+
+        bool validNum1 = double.TryParse(parts[0], out num1);
+        bool validNum2 = double.TryParse(parts[2], out num2);
+
+        if (validNum1 && validNum2)
+        {
+            string operation = parts[1];
+
+            if (operation == "+")
+            {
+                Console.WriteLine(num1 + " + " + num2 + " is " + (num1 + num2));
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(num1 + " - " + num2 + " is " + (num1 - num2));
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(num1 + " * " + num2 + " is " + (num1 * num2));
+            }
+            else if (operation == "/")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("You cannot divide by zero.");
+                }
+                else
+                {
+                    Console.WriteLine(num1 + " / " + num2 + " is " + (num1 / num2));
+                }
+            }
+            else if (operation == "%")
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine("You cannot use modulus by zero.");
+                }
+                else
+                {
+                    Console.WriteLine(num1 + " % " + num2 + " is " + (num1 % num2));
+                }
+            }
+            else
+            {
+                Console.WriteLine("I do not know how to do that.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("I do not know how to do that.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("I do not know how to do that.");
+    }
+}
+            
+
---
+++ homework/march_9/project/Array/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = [256,20,68,53,11];
+        
+        int smallest = numbers[0];
+        int biggest = numbers[0];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+
+            if (numbers[i] > biggest)
+            {
+                biggest = numbers[i];
+            }
+        }
+        
+        Console.WriteLine("Smallest number is: " + smallest);
+        Console.WriteLine("Biggest number is: " + biggest);
+    }
+}
+
+
